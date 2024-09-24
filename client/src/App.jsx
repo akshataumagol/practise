7a772c62e9a5a0f483b2baeff573ce3933ca948c
aartimahamune <aartimahamune5@gmail.com>
2024-09-24 21:52:45 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles/App.css';
+
+export default function App() {
+  return (
+    <div className='container'>
+      <p>Hover here</p>
+    <div className='text'>Hello World!</div>
+    </div>
+  )
+}
